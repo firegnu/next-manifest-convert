5f725e103f06edf8a60863ff9994e2dd8af67652
xin.jin <xin.jin@kaiostech.com>
2020-11-04 15:33:24 +0800

    repo init

---
+++ pages/index.js
@@ -0,0 +1,110 @@
+import utilStyles from '../styles/utils.module.css';
+
+const modifyFields = (oldManifest) => {
+  if (/^[\],:{}\s]*$/.test(oldManifest.value.replace(/\\["\\\/bfnrtu]/g, '@').
+  replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+  replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+    const oriManifest = JSON.parse(oldManifest.value);
+    let newManifest = {};
+    // the same keys and values
+    const pwaFields = [
+      'name',
+      'short_name',
+      'description',
+      'theme_color',
+      'background_color',
+      'orientation',
+    ];
+
+    pwaFields.forEach((key) => {
+      if (key in oriManifest) {
+        newManifest[key] = oriManifest[key];
+        delete oriManifest[key];
+      }
+    });
+
+    // get default_loacle
+    let defaultLocale = '';
+
+    // default_locale -> lang
+    if ('default_locale' in oriManifest) {
+      defaultLocale = oriManifest['default_locale'];
+      newManifest['lang'] = oriManifest['default_locale'];
+      delete oriManifest['default_locale'];
+    } else {
+      defaultLocale = 'en-US';
+      newManifest['lang'] = 'en-US';
+    }
+
+    // transfer name and description fields of default locale and remove locales
+    if ('locales' in oriManifest) {
+      newManifest['name'] = oriManifest['locales'][defaultLocale].name;
+      newManifest['description'] =
+        oriManifest['locales'][defaultLocale].description;
+      delete oriManifest['locales'];
+    }
+
+    // launch_path -> start_url
+    if ('launch_path' in oriManifest) {
+      newManifest['start_url'] = oriManifest['launch_path'];
+      delete oriManifest['launch_path'];
+    }
+    // icons
+    if ('icons' in oriManifest) {
+      const iconsObj = oriManifest['icons'];
+      let iconsResult = [];
+
+      Object.keys(iconsObj).reduce((pre, item) => {
+        const extname = iconsObj[item].split('.').pop();
+        const newFormat = {
+          src: iconsObj[item],
+          type: `image/${extname}`,
+          sizes: `${item}x${item}`,
+        };
+        pre.push(newFormat);
+        return pre;
+      }, iconsResult);
+      newManifest['icons'] = iconsResult;
+      delete oriManifest['icons'];
+    }
+    if(Object.keys(oriManifest).length > 0) {
+      newManifest['b2g_features'] = oriManifest;
+      // add core property and version to manifest b2g_features item
+      if('type' in oriManifest) {
+        const appType = oriManifest['type'];
+        if(appType === 'certified' || appType === 'priviliged') {
+          newManifest['b2g_features']['core'] = true;
+        }
+      }
+      newManifest['b2g_features']['version'] = '2.2';
+    }
+    const pretty = JSON.stringify(newManifest, undefined, 2);
+    document.getElementById('newmanifest').value = pretty;
+  }else{
+    document.getElementById('newmanifest').value = 'Your manifest format are wrong! Please check it!';
+  }
+}
+
+const handleClick = (e) =>  {
+  modifyFields(document.getElementById('orimanifest'));
+}
+
+function Home() {
+  return (
+    <div className={utilStyles.convertcontainer}>
+      <div className={utilStyles.converttextarea}>
+        <textarea className={utilStyles.convertcontent} id="orimanifest">
+        </textarea>
+      </div>
+      <div className={utilStyles.converBtn} onClick={handleClick}>
+        Convert
+      </div>
+      <div className={utilStyles.converttextarea}>
+        <textarea className={utilStyles.convertcontent} id="newmanifest">
+        </textarea>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
