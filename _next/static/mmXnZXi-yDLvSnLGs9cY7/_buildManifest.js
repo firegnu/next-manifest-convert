cad059f21ae4d4179cc33bd2b4ab6d8aeed3c7f0
xin.jin <xin.jin@kaiostech.com>
2020-11-10 14:39:28 +0800

    Updates

---
+++ _next/static/mmXnZXi-yDLvSnLGs9cY7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fb00dcc5b2e712e95c418.css","static\u002Fchunks\u002Fpages\u002Findex-a9bdafbdd43cb8b4e9d4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
