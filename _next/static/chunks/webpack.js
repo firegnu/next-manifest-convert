1bcce56092b5cf3223c2e521e151e3e6003f8704
xin.jin <xin.jin@kaiostech.com>
2020-11-10 17:47:29 +0800

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "47d9418adfa3b04a1e83";
+/******/ 	var hotCurrentHash = "31c05d6bfb6c82788e5f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
