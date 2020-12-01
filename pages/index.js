08fd9bda3ba224d26e6805886831dd7b0e5843a3
xin.jin <xin.jin@kaiostech.com>
2020-12-01 10:16:46 +0800

    Add upload API.

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,49 @@
-import { validateJson } from '../utils/utils';
+import React from 'react'
+import axios, { post } from 'axios';
 
-import convert from "../utils/convert";
-import validate from "../utils/validate";
+class SimpleReactFileUpload extends React.Component {
 
-import utilStyles from '../styles/utils.module.css';
+  constructor(props) {
+    super(props);
+    this.state ={
+      file:null
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
+  }
+  onFormSubmit(e){
+    e.preventDefault() // Stop form submit
+    this.fileUpload(this.state.file).then((response)=>{
+      console.log(response.data);
+    })
+  }
+  onChange(e) {
+    this.setState({file:e.target.files[0]})
+  }
+  fileUpload(file){
+    const url = '/api/user';
+    const formData = new FormData();
+    formData.append('file',file)
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    }
+    return  post(url, formData,config)
+  }
 
-const validateFields = (oldManifest) => {
-  if (validateJson(oldManifest)) {
-    const validateResults = validate(JSON.parse(oldManifest.value));
-    const pretty = JSON.stringify(validateResults, undefined, 2);
-    document.getElementById('newmanifest').value = pretty;
-  } else {
-    document.getElementById('newmanifest').value = 'Your 2.5 Manifest format is incorrect! Please check and try again！';
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" onChange={this.onChange} />
+        <button type="submit">Upload</button>
+      </form>
+    )
   }
 }
 
-const convertFields = (oldManifest) => {
-  if (validateJson(oldManifest)) {
-    const newManifest = convert(JSON.parse(oldManifest.value));
-    const pretty = JSON.stringify(newManifest, undefined, 2);
-    document.getElementById('newmanifest').value = pretty;
-  }else{
-    document.getElementById('newmanifest').value = 'Your 2.5 Manifest format is incorrect! Please check and try again！';
-  }
-}
 
-const convertManifest = (e) =>  {
-  convertFields(document.getElementById('orimanifest'));
-}
 
-const validateManifest = (e) => {
-  validateFields(document.getElementById('orimanifest'));
-}
-
-const prettyManifest = (e) => {
-  const oriManifest = document.getElementById('orimanifest');
-  if (validateJson(oriManifest)) {
-    const pretty = JSON.stringify(JSON.parse(oriManifest.value), undefined, 2);
-    oriManifest.value = pretty;
-  }else{
-    document.getElementById('newmanifest').value = 'Your 2.5 Manifest format is incorrect! Please check and try again！';
-  }
-}
-
-function Home() {
-  return (
-    <>
-     <div>
-       <h1 className={utilStyles.headerTitle}>Convert&Validate Manifest from KaiOS2.5.4 to KaiOS3.0</h1>
-     </div>
-     <div className={utilStyles.convertcontainer}>
-      <div className={utilStyles.converttextarea}>
-        <div className={utilStyles.prettyWrapper}>
-          <h3>2.5.4 Manifest</h3>
-          <div className={utilStyles.buttonPretty} onClick={prettyManifest}>
-            Pretty
-          </div>
-        </div>
-        <textarea className={utilStyles.convertcontent} id="orimanifest">
-        </textarea>
-      </div>
-       <div className={utilStyles.buttonWrapper}>
-          <div className={utilStyles.buttonConvert} onClick={convertManifest}>
-            CONVERT
-          </div>
-           <div className={utilStyles.buttonValidate} onClick={validateManifest}>
-             VALIDATE
-           </div>
-       </div>
-      <div className={utilStyles.converttextarea}>
-        <h3>3.0 Manifest</h3>
-        <textarea className={utilStyles.convertcontent} id="newmanifest" readOnly>
-        </textarea>
-      </div>
-    </div>
-      </>
-  )
-}
-
-export default Home;
+export default SimpleReactFileUpload
