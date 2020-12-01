0ac7f2f5c7f5cc2212909a09325ba74ed17afb74
xin.jin <xin.jin@kaiostech.com>
2020-12-01 10:22:04 +0800

    Modify manifest vonvert page title.

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ class SimpleReactFileUpload extends React.Component {
   render() {
     return (
       <form onSubmit={this.onFormSubmit}>
-        <h1>File Upload</h1>
+        <h1>Please Upload your 2.5 manifest</h1>
         <input type="file" onChange={this.onChange} />
         <button type="submit">Upload</button>
       </form>
