e99396a0ea2906ddda1546a3b967e8051f4ce10b
xin.jin <xin.jin@kaiostech.com>
2020-12-18 16:30:28 +0800

    modify page's style.

--- Components/header.js
+++ Components/header.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="bg-purple-800 shadow-md z-50 w-full px-5 flex justify-between items-center">
+    <header className="bg-purple-800 shadow-md z-50 w-full px-5 flex items-center">
       <img className="h-24 w-24" src="/logo.svg" alt="convert usage" />
+      <p
+        className="text-lg font-bold leading-relaxed inline-block ml-7 mr-4 py-2 whitespace-no-wrap uppercase text-white"
+      >
+        Next Manifest Convertor
+      </p>
     </header>
   )
 }
